bc323b3b0f1317677a2a14995a35bfd2798ee730
Rigelubb <134905955+Rigelubb@users.noreply.github.com>
2023-05-29 01:19:26 -0400

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -1,71 +1,90 @@
-#include <iostream>
+// cliente.cpp
 
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 
-#include <arpa/inet.h>
+const char* IP_SERVIDOR = "127.0.0.1";
+const int PUERTO = 7777;
+const int TAMANIO_TABLERO = 15;
 
-#include <unistd.h>
+enum class EstadoCasilla {
+    Vacio,
+    Barco,
+    Disparada
+};
 
-#include <string>
+void imprimirTablero(const std::string& tablero) {
+    for (int fila = 0; fila < TAMANIO_TABLERO; ++fila) {
+        for (int columna = 0; columna < TAMANIO_TABLERO; ++columna) {
+            std::cout << tablero[fila * TAMANIO_TABLERO + columna] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
 
+void enviarMensaje(int socket, const std::string& mensaje) {
+    int longitudMensaje = mensaje.length();
+    send(socket, &longitudMensaje, sizeof(longitudMensaje), 0);
+    send(socket, mensaje.c_str(), longitudMensaje, 0);
+}
 
-int main(int argc, char * argv[]) {
-  // Verificar argumentos de línea de comandos
-  if (argc != 3) {
-    std::cout << "Uso: " << argv[0] << " <ip_servidor> <puerto_servidor>" << std::endl;
-    return 1;
-  }
+std::string recibirMensaje(int socket) {
+    int longitudMensaje;
+    recv(socket, &longitudMensaje, sizeof(longitudMensaje), 0);
+    char buffer[longitudMensaje + 1];
+    recv(socket, buffer, longitudMensaje, 0);
+    buffer[longitudMensaje] = '\0';
+    return buffer;
+}
 
-  std::string ipServidor = argv[1];
-  int puertoServidor = std::stoi(argv[2]);
-
-  int socketCliente = crearSocket();
-  conectarSocket(socketCliente, ipServidor, puertoServidor);
-
-  std::string mensaje;
-
-  // Conexión establecida, comenzar interacción con el servidor
-  while (true) {
-    // Leer y enviar coordenadas de disparo
-    std::cout << "Ingrese las coordenadas de disparo (ejemplo: A1): ";
-    std::getline(std::cin, mensaje);
-
-    if (mensaje == "salir") {
-      break;
+int main() {
+    int socketCliente = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketCliente == -1) {
+        std::cerr << "Error al crear el socket" << std::endl;
+        return 1;
     }
 
-    enviarMensaje(socketCliente, mensaje);
+    sockaddr_in direccionServidor{};
+    direccionServidor.sin_family = AF_INET;
+    direccionServidor.sin_port = htons(PUERTO);
+    if (inet_pton(AF_INET, IP_SERVIDOR, &(direccionServidor.sin_addr)) <= 0) {
+        std::cerr << "Dirección inválida/No soportada" << std::endl;
+        return 1;
+    }
 
-    // Recibir y mostrar resultados del disparo
-    mensaje = recibirMensaje(socketCliente);
-    std::cout << "Resultado del disparo: " << mensaje << std::endl;
+    if (connect(socketCliente, (struct sockaddr*)&direccionServidor, sizeof(direccionServidor)) == -1) {
+        std::cerr << "Error al conectar al servidor" << std::endl;
+        return 1;
+    }
 
-    // Recibir y mostrar tableros actualizados
-    mensaje = recibirMensaje(socketCliente);
-    std::cout << "Tableros actualizados:\n" << mensaje << std::endl;
-  }
+    std::string posicionesEmbarcaciones = recibirMensaje(socketCliente);
+    imprimirTablero(posicionesEmbarcaciones);
 
-  cerrarSocket(socketCliente);
+    while (true) {
+        std::cout << "Ingresa las coordenadas del disparo (fila columna): ";
+        std::string coordenadas;
+        std::cin >> coordenadas;
 
-  return 0;
-}
+        enviarMensaje(socketCliente, coordenadas);
 
-void solicitarDisparo() {
-  std::string coordenadas;
-  std::cout << "Ingrese las coordenadas del disparo (ejemplo: A1): ";
-  std::cin >> coordenadas;
+        std::string resultadoDisparo = recibirMensaje(socketCliente);
+        std::string tablerosActualizados = recibirMensaje(socketCliente);
 
-  // Enviar las coordenadas al servidor
-  enviarMensaje(coordenadas);
-}
+        imprimirTablero(tablerosActualizados);
+        std::cout << "Resultado del disparo: " << resultadoDisparo << std::endl;
 
-void solicitarDisparo() {
-    std::string coordenadas;
-    std::cout << "Ingrese las coordenadas del disparo (ejemplo: A1): ";
-    std::cin >> coordenadas;
+        if (resultadoDisparo == "Hundido") {
+            std::cout << "¡Ganaste!" << std::endl;
+            break;
+        }
+    }
 
-    // Enviar las coordenadas al servidor
-    enviarMensaje(coordenadas);
+    close(socketCliente);
+    return 0;
 }
 
 
